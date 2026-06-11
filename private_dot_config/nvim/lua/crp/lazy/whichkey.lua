07eafd4531982ff45d4a84e005ae90a95517bbfd
Cristiano Prato <cristiano.prato@egluetechnologies.com>
2026-06-11 17:07:39 +0200

    update

--- private_dot_config/nvim/lua/crp/lazy/whichkey.lua
+++ private_dot_config/nvim/lua/crp/lazy/whichkey.lua
@@ -6,5 +6,37 @@ return {
     vim.o.timeoutlen = 300
   end,
   opts = {
+    preset = "helix",
+    delay = 100,
+    icons = {
+      breadcrumb = "»",
+      separator = "🌵",
+      group = "+",
+    },
+    plugins = {
+      marks = true,
+      registers = true,
+      spelling = {
+        enabled = true,
+        suggestions = 20,
+      },
+      presets = {
+        operators = false,
+        motions = false,
+        text_objects = false,
+        windows = true,
+        nav = true,
+        z = true,
+        g = true,
+      },
+    },
+    layout = {
+      align = "left",
+      height = { min = 4, max = 25 },
+      spacing = 3,
+      width = { min = 20, max = 50 },
+    },
+    show_help = true,
+    show_keys = true,
   }
 }
