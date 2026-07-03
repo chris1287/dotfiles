9493f6efe79139a3bbd8763167ba3e350a0340ec
Cristiano Prato <cristiano.prato@egluetechnologies.com>
2026-07-03 09:23:14 +0200

    update

--- private_dot_config/nvim/lua/crp/lazy/treesitter.lua
+++ private_dot_config/nvim/lua/crp/lazy/treesitter.lua
@@ -1,11 +1,12 @@
 return {
     "nvim-treesitter/nvim-treesitter",
+    branch = "master",
     build = ":TSUpdate",
     config = function()
         require("nvim-treesitter.configs").setup({
             -- A list of parser names, or "all"
             ensure_installed = {
-                "c", "cpp", "lua", "rust", "bash", "markdown", "python"
+                "c", "cpp", "lua", "rust", "bash", "python", "markdown",
             },
         })
     end
